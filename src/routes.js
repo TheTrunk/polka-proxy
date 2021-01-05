bb372f4f8f65fc5ba46bb0880a105e63ac9405ff
Tadeas Kmenta <tadeas.kmenta@zel.cash>
2021-01-05 11:02:25 +0700

    submittx call

--- src/routes.js
+++ src/routes.js
@@ -4,4 +4,7 @@ module.exports = (app) => {
     app.get('/metadata', function (req, res) {
         polka.metadata(req, res)
     })
+    app.post('/submittx', function (req, res) {
+        polka.submittx(req, res)
+    })
 };
--- src/services/polka.js
+++ src/services/polka.js
@@ -2,6 +2,23 @@ const log = require('../lib/log');
 const config = require('config');
 const axios = require("axios");
 
+function performNodeRequest(res, apiURL, data) {
+    const axiosConfig = {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+    axios
+        .post(apiURL, JSON.stringify(data), axiosConfig)
+        .then(response => {
+            res.json(response.data)
+        })
+        .catch(error => {
+            log.error(error);
+            res.status(500).send('An error has been encountered while processing request.')
+        });
+}
+
 function metadata(req, res) {
     const max = 1000000;
     const min = 1;
@@ -13,24 +30,34 @@ function metadata(req, res) {
         params: [],
         id: Math.floor(Math.random() * (max - min + 1)) + min
     };
-    const axiosConfig = {
-        headers: {
-            "Content-Type": "application/json",
-        },
-    };
-    axios
-        .post(apiUrl, JSON.stringify(data), axiosConfig)
-        .then(response => {
-            // console.log(response)
-            res.json(response.data)
-        })
-        .catch(error => {
-            log.error(error);
-            res.status(500).send('An error has been encountered while processing request.')
-        });
+    performNodeRequest(res, apiUrl, data);
+}
+
+function submittx(req, res) {
+    let body = '';
+    req.on('data', function (data) {
+        body += data;
+    });
+
+    req.on('end', function () {
+        const processedBody = JSON.parse(body)
+        const { rawtx } = processedBody;
+        const max = 1000000;
+        const min = 1;
+
+        const apiUrl = `http://${config.server.address}:${config.server.port}`;
+        const data = {
+            jsonrpc: "2.0",
+            method: "author_submitExtrinsic",
+            params: [rawtx],
+            id: Math.floor(Math.random() * (max - min + 1)) + min
+        };
+        performNodeRequest(res, apiUrl, data);
+    });
 }
 
 
 module.exports = {
     metadata,
+    submittx
 }
